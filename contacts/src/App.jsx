150b11998cc7cf642f1dd0b67038310dab29c6f8
serhat-bca <sersen@bergen.org>
2026-04-21 14:51:59 -0400

    contact

---
+++ contacts/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h2>Contacts</h2>
+      <Contact name="John Wick" dob="1981" state="IL" />
+      <Contact name="Neo" dob="1980" state="NY" />
+    </div>
+  );
+};
+
+export default App;
