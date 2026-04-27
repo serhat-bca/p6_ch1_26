fa693c6ddedce938999811c3e1c7ffab66f0b2cb
serhat-bca <sersen@bergen.org>
2026-04-27 13:04:27 -0400

    arrow function syntax showed.

--- counter-state/src/App.jsx
+++ counter-state/src/App.jsx
@@ -1,5 +1,11 @@
 import { useState } from "react";
 
+// const Counter = ({ counter }) => {
+//   return <p>Counter {counter}</p>;
+// };
+
+const Counter = ({ counter }) => <p>Counter: {counter}</p>;
+
 const App = () => {
   const [counter, setCounter] = useState(0);
 
@@ -11,15 +17,19 @@ const App = () => {
     setCounter(counter + count);
   };
 
+  const resetCounter = () => {
+    setCounter(0);
+  };
+
   return (
     <div>
       <h3>Counter with useState</h3>
-      <p>Counter: {counter}</p>
+      <Counter counter={counter} />
       <button onClick={increaseCounter}>+</button>
-      <button>-</button>
-      <button>reset</button>
+      <button onClick={() => updateCounter(-1)}>-</button>
+      <button onClick={resetCounter}>reset</button>
       <button onClick={() => updateCounter(10)}>+10</button>
-      <button>-10</button>
+      <button onClick={() => updateCounter(-10)}>-10</button>
     </div>
   );
 };
